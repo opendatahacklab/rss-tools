8a5b275c0f29f90f5c695ad5314a537668437df0
cristianolongo <cristianolongo@gmail.com>
2017-06-11 15:48:19 +0200

    better js license

--- js/rss-tools.js
+++ js/rss-tools.js
@@ -7,18 +7,27 @@
  *
  * Copyright 2017 Cristiano Longo
  *
- * rss-tools is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * rss-tools is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @licstart  The following is the entire license notice for the 
+ * JavaScript code in this page.
+ * 
+ * Copyright 2017 Cristiano Longo
+ * 
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms  of the GNU Lesser General 
+ * Public License as published by the Free Software Foundation, either version 
+ * 3 of the License, or  (at your option) any later version. The code is distributed 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ * 
+ * As additional permission under GNU GPL version 3 section 7, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 4, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.   
+ * 
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
  */
 
 /**
